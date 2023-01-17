22452b20796a0d3114cfb6095999a0c7b3b2beae
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2023-01-17 21:14:47 +0100

    vermin says 2.4, 3.0

--- pandoc_kroki_filter.py
+++ pandoc_kroki_filter.py
@@ -7,7 +7,7 @@ import zlib
 from pandocfilters import Image, Para, get_caption, toJSONFilter
 
 # Diagram types that will be supported.
-DIAGRAM_TYPES: list[str] = [
+DIAGRAM_TYPES = [
     "blockdiag",
     "bpmn",
     "bytefield",
@@ -30,15 +30,15 @@ DIAGRAM_TYPES: list[str] = [
     "vegalite",
     "wavedrom",
 ]
-DIAGRAM_SYNONYMNS: dict[str, str] = {
+DIAGRAM_SYNONYMNS = {
     "dot": "graphviz",
     "c4": "c4plantuml",
     "vega-lite": "vegalite",
 }
-AVAILABLE_DIAGRAMS: list[str] = DIAGRAM_TYPES + list(DIAGRAM_SYNONYMNS.keys())
+AVAILABLE_DIAGRAMS = DIAGRAM_TYPES + list(DIAGRAM_SYNONYMNS.keys())
 
 # List of diagrams types the user chooses not to process
-DIAGRAM_BLACKLIST: list[str] = list(
+DIAGRAM_BLACKLIST = list(
     filter(
         lambda d: d in AVAILABLE_DIAGRAMS,
         os.environ.get("KROKI_DIAGRAM_BLACKLIST", "").split(","),
@@ -46,16 +46,14 @@ DIAGRAM_BLACKLIST: list[str] = list(
 )
 
 # kroki server to point to
-KROKI_SERVER: str = os.environ.get("KROKI_SERVER", "https://kroki.io/")
+KROKI_SERVER = os.environ.get("KROKI_SERVER", "https://kroki.io/")
 KROKI_SERVER = KROKI_SERVER[:-1] if KROKI_SERVER[-1] == "/" else KROKI_SERVER
 
 
 def kroki(key, value, format_, _):
     if key == "CodeBlock":
         [[ident, classes, keyvals], content] = value
-        diagram_classes: list[str] = list(
-            set(AVAILABLE_DIAGRAMS) & set(classes)
-        )
+        diagram_classes = list(set(AVAILABLE_DIAGRAMS) & set(classes))
         if (
             len(diagram_classes) == 1
             and diagram_classes[0] not in DIAGRAM_BLACKLIST
@@ -64,20 +62,20 @@ def kroki(key, value, format_, _):
 
             # Divine the correct diagram type to use with kroki
             if diagram_classes[0] in DIAGRAM_SYNONYMNS.keys():
-                diagram_type: str = DIAGRAM_SYNONYMNS[diagram_classes[0]]
+                diagram_type = DIAGRAM_SYNONYMNS[diagram_classes[0]]
             else:
                 diagram_type = diagram_classes[0]
 
             # create the url to the kroki diagram and link as an image
-            encoded: str = base64.urlsafe_b64encode(
+            encoded = base64.urlsafe_b64encode(
                 zlib.compress(content.encode("utf-8"), 9)
             ).decode()
-            url: str = f"{KROKI_SERVER}/{diagram_type}/svg/{encoded}"
+            url = KROKI_SERVER + "/" + diagram_type + "/svg/" + encoded
 
             return Para([Image([ident, [], keyvals], caption, [url, typef])])
 
 
-def main() -> None:
+def main():
     toJSONFilter(kroki)
 
 
